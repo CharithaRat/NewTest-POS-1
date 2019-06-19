005c6372743333fb58162ee89f1a1f833559f532
CharithaRat <charitharathnayaka@gmail.com>
2019-06-20 01:05:13 +0530

    Aded itemservice class to FrontEnd

--- FrontEnd/src/app/view/manage-item/manage-item.component.ts
+++ FrontEnd/src/app/view/manage-item/manage-item.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {Item} from "../../dto/item";
+import {ItemService} from "../../service/item.service";
 
 @Component({
   selector: 'app-manage-item',
   templateUrl: './manage-item.component.html',
   styleUrls: ['./manage-item.component.css']
 })
+
 export class ManageItemComponent implements OnInit {
 items:Item[]=[];
 
-  constructor() { }
+  constructor(private itemService:ItemService) {
+  }
 
   ngOnInit() {
+    this.itemService.getAllItems().subscribe(items=>{
+      this.items = items;
+    });
   }
 
 }
